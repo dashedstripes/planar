7aee06617c17d90902a89957070f56949039e793
Adam Gray <adam@pateo.co>
2018-09-09 14:35:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Planar
+
+A flat file CMS.
+
+## What is this?
+
+This is a database-less (I think that's a word) content management system. I wanted a way to create simple websites with dynamic content, without having to install a huge wordpress instance or some other content management system. This project is perfect for small sites without too much content, but enough to make it worthwhile having objects dynamic rather than hardcoded.
+
+## Getting started
+
+Install dependencies with `yarn install`.
+Run the dev server with `node index.js`, or if you have nodemon, `nodemon index.js`
+
+## Credits
+
+The artwork images are by Kate Yanish, you can see more of her work at her [website](https://kateyanish.com)
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const glob = require('glob')
+const app = express()
+
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('index', { title: 'Hey', message: 'Hello there!' }))
+
+glob.sync('./schema/*.json').forEach((schemaFile) => {
+  let schema = require(schemaFile)
+  let objects = []
+
+  glob.sync(`./objects/${schema.id}/*.json`).forEach((objectFile) => {
+    let object = require(objectFile)
+    objects.push(object)
+
+    app.get(`${schema.url}/${object.id}`, (req, res) => res.render(`${schema.id}/single`, { object }))
+  })
+
+  app.get(schema.url, (req, res) => res.render(`${schema.id}/list`, { schema, objects }))
+})
+
+app.listen(3000, () => console.log('http://localhost:3000'))
